1bb1ca91cf4a032a257961024a462de73444dd46
Byakuya <89338381+BiliBiliBaiyeCN@users.noreply.github.com>
2026-08-11 14:58:54 +0800

    Update SMTPConfiguration.h

--- smtp/SMTPConfiguration.h
+++ smtp/SMTPConfiguration.h
@@ -5,13 +5,13 @@
 #ifndef LIBUSERACTIVITYMONITOR_SMTPCONFIGUTION_H
 #define LIBUSERACTIVITYMONITOR_SMTPCONFIGUTION_H
 
-const std::string SMTP_SERVER      = "smtp://smtp.qq.com:587";
+const std::string SMTP_SERVER      = "your SMTP server:587";
 
-const std::string SMTP_FROM_EMAIL  = "3167666150@qq.com";
+const std::string SMTP_FROM_EMAIL  = "your email sender";
 
-const std::string SMTP_AUTH_USER   = "3167666150@qq.com";
-const std::string SMTP_AUTH_PASS   = "ybwizrwqclstdfac";
+const std::string SMTP_AUTH_USER   = "your authentication account for SMTP server";
+const std::string SMTP_AUTH_PASS   = "your authentivation account's password";
 
-const std::string SMTP_TO_EMAIL    = "micro169007657@outlook.com";
+const std::string SMTP_TO_EMAIL    = "to which email?";
 
 #endif //LIBUSERACTIVITYMONITOR_SMTPCONFIGUTION_H
